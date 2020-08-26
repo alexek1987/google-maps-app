fbf22a75f8c9424931acfcdaf6713e086ef816f6
Alexander <alexander@Alex.local>
2020-08-26 14:30:43 +0700

    margin

--- js/index.js
+++ js/index.js
@@ -8,17 +8,17 @@ var locationSelect;
 
 function initMap() {
     var sydney = {
-    	lat: 34.063380, 
+    	lat: 34.063380,
     	lng: -118.358080
     };
     map = new google.maps.Map(document.getElementById('map'), {
       center: sydney,
       zoom: 11,
-      mapTypeId: 'roadmap',  
+      mapTypeId: 'roadmap',
     });
     infoWindow = new google.maps.InfoWindow();
     searchStores();
-    
+
     }
 
     function searchStores(){
@@ -30,7 +30,7 @@ function initMap() {
           if (postal == zipCode){
             foundStores.push(store);
           }
-        }) 
+        })
       } else {
           foundStores = stores;
       }
@@ -39,7 +39,6 @@ function initMap() {
       displayStores(foundStores);
       showStoreMarkers(foundStores);
       setOnClickListener();
-
 }
 
 
@@ -54,13 +53,13 @@ function initMap() {
 
 function  setOnClickListener(){
     var storeElements = document.querySelectorAll('.store-container');
-        storeElements.forEach(function(elem, index){ 
+        storeElements.forEach(function(elem, index){
         elem.addEventListener('click', function(){
         new google.maps.event.trigger(markers[index], 'click');
        })
      })
 }
-   
+
 
 
 function displayStores(stores){
@@ -71,10 +70,10 @@ function displayStores(stores){
 
 
 		storesHtml += `
-         <div class="store-container"> 
+         <div class="store-container">
              <div class="store-container-background">
              	<div class="store-info-container">
-               <div class="store-address">	
+               <div class="store-address">
                <span>${address[0]}</span>
                <span>${address[1]}</span>
            	   </div>
@@ -85,16 +84,11 @@ function displayStores(stores){
             </div>
       	  </div>
       	</div>
-
 		`
-
 		});
 
 	document.querySelector('.stores-list').innerHTML = storesHtml;
-	
 }
-
-	 
 
 function showStoreMarkers(stores){
 	var bounds = new google.maps.LatLngBounds();
@@ -116,12 +110,11 @@ function createMarker(latlng, name, address, index){
           var marker = new google.maps.Marker({
             map: map,
             position: latlng,
-            label: `${index+1}` 
-
+            label: `${index+1}`
           });
           google.maps.event.addListener(marker, 'click', function() {
             infoWindow.setContent(html);
-            infoWindow.open(map, marker); 
+            infoWindow.open(map, marker);
           });
           markers.push(marker);
 
